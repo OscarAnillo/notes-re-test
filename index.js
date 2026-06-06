34b4444ef694fb54a6ca5d431b33f13138451337
Oscar Anillo <oscarani14@gmail.com>
2026-06-05 20:09:44 -0500

    add new features

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json()); //This is for post requests!
 
+/* GET Requests */
 app.get("/", (req, res) => {
   res.send("<h1>Hello World! This is a refresh!</h1>");
 });
@@ -19,51 +20,54 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  note ? res.json(note) : res.status(404).end();
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(next);
 });
 
+/* POST Request */
 app.post("/api/notes", (req, res) => {
-  const note = req.body;
-  if (!note.content) {
+  const body = req.body;
+
+  if (!body.content) {
     return res.status(400).json({ error: "content missing" });
   }
 
-  const newNote = {
-    content: note.content,
+  const newNote = new Note({
+    content: body.content,
     important: Math.random() > 0.5,
-  };
-  notes = notes.concat(newNote);
-  res.json(newNote);
+  });
+
+  newNote.save().then((savedNote) => res.json(savedNote));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((note) => {
+      res.status(204).end();
+    })
+    .catch(next);
 });
 
+/* PATCH Request */
 app.patch("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
   const { important } = req.body;
 
-  const note = notes.find((note) => note.id === id);
-  if (!note) {
-    return res.status(404).json({
-      error: "Note was not found",
-    });
-  }
-  note.important = important;
-  res.json(note);
+  Note.findByIdAndUpdate(
+    req.params.id,
+    { important },
+    { new: true, runValidators: true },
+  ).then((updatedNote) => {
+    res.json(updatedNote);
+  });
 });
-
-const unknownEndpoint = (_, res) => {
-  res.status(404).send({ error: "Unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
